8b6c25847613f722d0918b446db87550fd8a69c0
luiz Boldrini <luizboldriniej@gmail.com>
2022-05-26 19:23:23 -0300

    fix: Organized contollers

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-  
-const app = express();
-
-app.use(express.json());
-app.use(require("./routes"));
-app.listen(process.env.PORT || 3000);
-
-module.exports = app;
