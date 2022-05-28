8b7cd0c0b7d7378cddc093524c7716944c600654
Félix Cândido <netocandido007@gmail.com>
2022-05-28 01:48:46 -0300

    feat(middleware): validation if cpf is real

--- src/middleware/validarFuncionario.js
+++ src/middleware/validarFuncionario.js
@@ -20,8 +20,8 @@ module.exports = async (req, res, next) => {
 	const reqBody = req.body;
 	const birthday = moment(reqBody.birthday, "DD/MM/YYYY").format("YYYY/MM/DD");
 	try {
-		if (!validaData(birthday)) {
-			throw new Error("Erro de validação");
+		if (!validaData(birthday) || !validaCpf(reqBody.cpf)) {
+			throw new Error("Campos Invalidos");
 		}
 		await authEmploer.validateAsync({...reqBody, birthday});
 		next();
@@ -40,4 +40,34 @@ function validaData(formatedDate) {
 }
 
 
- 
+function validaCpf(cpf) {
+	let soma = 0;
+
+	if(cpf === "01234567890") return false;
+
+	for(let i = 0; i <= 9; i++) {
+		let numerosRepetidos = `${i}${i}${i}${i}${i}${i}${i}${i}${i}${i}${i}`;
+		if(cpf === numerosRepetidos) return false;
+	}
+
+	for(let i = 1; i <= 9; i++) {
+		soma += parseInt(cpf.substring(i-1, i)) * (11 - i);
+	} 
+	let resto = (soma * 10) % 11;
+
+	if((resto == 10) || (resto == 11)) {
+		resto = 0;
+	}
+	if(resto != parseInt(cpf.substring(9, 10))) return false;
+
+	soma = 0;
+	for(let i = 1; i<= 10; i++) {
+		soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
+	}
+	resto = (soma * 10) % 11;
+	if((resto == 10) || (resto == 11)) {
+		resto = 0;
+	}
+	if(resto != parseInt(cpf.substring(10, 11))) return false;
+	return true;
+}
