741813983e8e654db8a3ec23382d12531353426c
Skyler-a <77992926+Skyler-a@users.noreply.github.com>
2022-05-29 11:52:26 -0300

    Update: updated product routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,7 +14,6 @@ router
 	.put("/api/v1/employee/:id",validarFuncionario, FuncionariosController.atualizaFuncionario)
 	.delete("/api/v1/employee/:id", FuncionariosController.apagaFuncionario)
 	.get("/api/v1/product", ProdutosController.listarProdutos)
-	.get("/api/v1/product", ProdutosController.listarProdutoPorName)
 	.post("/api/v1/product",validarProduto, ProdutosController.criarProduto);
 
 module.exports = router;
